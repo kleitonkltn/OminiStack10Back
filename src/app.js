4001929c7596c0876531ff57ee95ea0c8c074706
unknown <kleitonkltn@gmail.com>
2020-01-28 01:12:09 -0300

    refactor(app.js and packages): added cors to control acess server

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 var express = require('express')
-var path = require('path')
+var cors = require('cors')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 const mongoose = require('mongoose')
@@ -14,12 +14,9 @@ mongoose.connect(process.env.DB_URL, {
     useCreateIndex: true
 })
 
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
-app.use(express.urlencoded({
-    extended: false
-}))
-app.use(cookieParser())
 
 app.use(routes)
 
